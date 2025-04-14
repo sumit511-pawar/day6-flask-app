00aea788f09ea46eddb5e2b76ba1672c6f3ff486
sumit pawar <pawarsumitp2000@gmail.com>
2025-04-13 21:03:45 -0700

    changes in code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Welcome to Season 2! You are learning GitHub Actions."
+    return "Hello, DevOps"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
